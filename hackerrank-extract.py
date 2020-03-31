d3a8f56dcbb60b9279f754b251a819debb6af0e0
tvidyadhari <41134419+tvidyadhari@users.noreply.github.com>
2020-04-01 03:45:28 +0530

    Update hackerrank-extract.py

--- hackerrank-extract.py
+++ hackerrank-extract.py
@@ -1,57 +1,103 @@
-# bs4 reference https://www.dataquest.io/blog/web-scraping-beautifulsoup/
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 import bs4
 import time
 import json
+from selenium.common.exceptions import NoSuchElementException
+# for codeforces
+import requests
 
-# returns the content(string) from the given url using selenium
+# input username, output set/list of successful submissions
+def codeforces_profile_submissions(username):
+    response = requests.get("https://codeforces.com/api/user.status", params={"handle":username})
+    response_json = response.json()
+    if response_json["status"] == "OK":
+        solved_problems = set()
+        verdict = set()
+        for item in response_json['result']:
+            if item['verdict'] == "OK":
+                solved_problems.add(item['problem']['name'])
+            verdict.add(item['verdict'])
+        return "solved_problems"
+    else:
+        return response_json
+
 def get_html_source(url):
     html_source = ""
-
-    # *******CHANGE THIS VALUE ACCORDINGLY*********
-    # chrome webdriver path (https://chromedriver.chromium.org/)
     path = '/Users/vidyadhari/Downloads/chromedriver' 
-
-    driver = webdriver.Chrome(executable_path=path)
+    chrome_options = Options()
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+    chrome_options.add_argument('user-agent={0}'.format(user_agent))
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    driver = webdriver.Chrome(options=chrome_options, executable_path=path)
     driver.get(url)
-
-    # allow the page to populate/ get info from API's/ XHR
     time.sleep(5)
+    driver.get_screenshot_as_file("capture.png")
 
-    # set number of items per page as 100
-    select = Select(driver.find_element_by_name('pagination-length'))
-    select.select_by_visible_text("100")
-    time.sleep(5)
-
-    # get total number of pages to run through
-    pagination_items = driver.find_element_by_class_name(
-        'pagination').find_elements_by_tag_name('li')
-    pagination_length = int(pagination_items[-3].text)
-    # next page button/ link
-    pagination_next = pagination_items[-2]
-    print(pagination_length)
-
-    # read the current page and click the next page button until all pages are read
-    for _ in range(pagination_length):
-        html_source += driver.page_source
-        # clicking the next page button/link
-        driver.find_element_by_class_name(
-            'pagination').find_elements_by_tag_name('li')[-2].click()
+    # check if pagination-length exists 
+    pagination_dropdown = checkIfElementExists(driver, 'pagination-length', 'name')
+    # if pagination_dropdown is not None
+    if pagination_dropdown:
+        select = Select(pagination_dropdown)
+        select.select_by_visible_text("100")
         time.sleep(5)
+
+        # check if pagination exists
+        pagination = checkIfElementExists(driver, 'pagination', 'class_name')
+        # if pagination is not None
+        if pagination:
+            pagination_items = pagination.find_elements_by_tag_name('li')
+            pagination_length = int(pagination_items[-3].text)
+        else:
+            pagination_length = 1
+
+        # loop changed to pagination_length - 1 since last page has no next button
+        for i in range(pagination_length - 1):
+            html_source += driver.page_source
+            driver.find_element_by_class_name(
+                'pagination').find_elements_by_tag_name('li')[-2].click()
+            time.sleep(5)
+        # get last page data
+        html_source += driver.page_source
+    # if pagination-length not found
+    else:
+        html_source += driver.page_source
     driver.quit()
     return html_source
 
+# METHOD TO CHECK IF ELEMENT EXISTS, IF EXISTS RETURN ELEMENT
+# func_type: element locating method used
+# name: name of the locator
+def checkIfElementExists(driver, name, func_type):
+    elem = None
+    if func_type == "name":
+        try:
+            elem = driver.find_element_by_name(name)
+        except NoSuchElementException:
+            return None
+    elif func_type == "class_name":
+        try:
+            elem = driver.find_element_by_class_name(name)
+        except NoSuchElementException:
+            return None
+    return elem
 
 
-# parses the html_source and returns json with each entry of the format
-# {
-#   rank: rank,
-#   username: username,
-#   score: score,
-#   time: time
-# }
-# using bs4
+
+
 def get_json(html_source):
     soup = bs4.BeautifulSoup(html_source, 'html.parser')
     leaderboard = soup.find_all('div', class_='leaderboard-list-view')
@@ -66,10 +112,10 @@ def get_json(html_source):
         }
         leaderboard_list.append(row_dict)
 
-    # print(leaderboard_list, file=open("output.txt", "w"))
     print(len(leaderboard_list))
     return json.dumps(leaderboard_list)
 
 
-url = 'https://www.hackerrank.com/contests/middle1/leaderboard/'
-print(get_json(get_html_source(url)))
+url = 'https://www.hackerrank.com/contests/middle1/leaderboard/1'
+# print(get_json(get_html_source(url)))
+print(codeforces_profile_submissions("navya570"))
